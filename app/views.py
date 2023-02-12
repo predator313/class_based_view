ec9502f5573535e154c41fb5731fca449217650f
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-12 22:50:52 +0530

    migrate

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+class Myview(View):
+    name='predator'
+    def get_name(self,request):
+        return HttpResponse(self.name)
+
+
+
