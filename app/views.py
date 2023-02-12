962b01efa4829083a2e59574de4831b76e9a3430
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-12 22:55:29 +0530

    get method is working

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 class Myview(View):
     name='predator'
-    def get_name(self,request):
+    def get(self,request):
         return HttpResponse(self.name)
 
 
